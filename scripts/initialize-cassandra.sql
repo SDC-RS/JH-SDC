65b6ba0a8e8d75e9858d5c5b65c9d42fae81769e
Jessica Hu <ruiruihu001@gmail.com>
2021-08-10 22:45:28 -0700

    create table

---
+++ scripts/initialize-cassandra.sql
@@ -0,0 +1,29 @@
+CREATE KEYSPACE sdc
+WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
+
+USE sdc;
+
+CREATE TABLE IF NOT EXISTS questions (
+  question_id int PRIMARY KEY,
+  product_id int,
+  question_body varchar,
+  question_date timestamp,
+  asker_name varchar,
+  asker_email varchar,
+  question_helpfulness int,
+  reported boolean
+);
+
+CREATE TABLE IF NOT EXISTS answers (
+  id int PRIMARY KEY,
+  question_id int,
+  body varchar,
+  date timestamp,
+  answerer_name varchar,
+  answerer_email varchar,
+  helpfulness int,
+  reported boolean,
+  photos varchar
+);
+
+describe tables;
---
+++ scripts/initialize-postgres.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE sdc;
+
+\c sdc;
+
+CREATE TABLE IF NOT EXISTS questions (
+  question_id INTEGER NOT NULL,
+  product_id INTEGER NOT NULL,
+  question_body VARCHAR(512) NOT NULL,
+  question_date TIMESTAMP NOT NULL,
+  asker_name VARCHAR(128) NOT NULL,
+  asker_email VARCHAR(128) NOT NULL,
+  question_helpfulness INTEGER NOT NULL,
+  reported BOOLEAN NOT NULL,
+  PRIMARY KEY (question_id)
+);
+
+CREATE TABLE IF NOT EXISTS answers (
+  id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  body VARCHAR(512) NOT NULL,
+  date TIMESTAMP NOT NULL,
+  answerer_name VARCHAR(128) NOT NULL,
+  answerer_email VARCHAR(128) NOT NULL,
+  helpfulness INTEGER NOT NULL,
+  reported BOOLEAN NOT NULL,
+  photos VARCHAR(1024) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+\dt;
